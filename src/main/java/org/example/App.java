f731919319c731e1a6e9af9765e227e6a3d40e90
GPabis <grzegorz.pabis.96@gmail.com>
2020-05-03 18:49:49 +0200

    Started implementing animalDesign

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,19 +1,15 @@
 package org.example;
 
+import org.example.animalDesign.IconIMG;
+import org.example.animalDesign.PersianCatIconIMG;
 import org.example.animalRace.Race;
 import org.example.animalRace.RaceFactory;
 import org.example.animalStatistics.*;
+
 
 public class App 
 {
     public static void main( String[] args )
     {
-        AnimalStats animalStats = new AnimalStats();
-        Race race = RaceFactory.createRace(AnimalRaceType.BritishShorthairCat);
-        System.out.println(race.getAnimalType());
-        System.out.println(race.getRaceName());
-        System.out.println(race.getRaceDiscription());
-        animalStats.createAnimalStats(StatsFactoryMaker.makeStatsFactory(race.getAnimalType()));
-        System.out.println(animalStats.getHealth().getStat());
     }
 }
---
+++ src/main/java/org/example/animalDesign/IconIMG.java
@@ -0,0 +1,9 @@
+package org.example.animalDesign;
+
+import java.awt.image.BufferedImage;
+
+public interface IconIMG {
+    void setIMG();
+    BufferedImage getIMG();
+    BufferedImage getResizedIMG(int newW, int newH);
+}
